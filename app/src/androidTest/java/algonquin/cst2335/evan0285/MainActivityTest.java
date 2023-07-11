3551ed7917efefff884d108d93eb5e286a383128
Chris <evan0285@algonquinlive.com>
2023-07-10 22:17:29 -0400

    finished week5 added javadocs and junit tests

---
+++ app/src/androidTest/java/algonquin/cst2335/evan0285/MainActivityTest.java
@@ -0,0 +1,168 @@
+package algonquin.cst2335.evan0285;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import static org.hamcrest.core.AllOf.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction appCompatEditText = onView( withId(R.id.editTextText)  );
+
+
+        appCompatEditText.perform(replaceText("12345"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.editTextText),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(pressImeActionButton());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.button), withText("LOGIN"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.textView),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    /**
+     * test unit to test if password matches case requirements
+     */
+    @Test
+    public void testFindMissingUpperCase(){
+
+        ViewInteraction appCompatEditText = onView( withId(R.id.editTextText));
+
+        appCompatEditText.perform(replaceText("password123#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testMissingLowerCase(){
+
+        ViewInteraction appCompatEditText = onView( withId(R.id.editTextText));
+
+        appCompatEditText.perform(replaceText("PASSWORD123#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testMissingDigit(){
+
+        ViewInteraction appCompatEditText = onView( withId(R.id.editTextText));
+
+        appCompatEditText.perform(replaceText("passsWithoutDigit"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testMissingSpecialChar(){
+
+        ViewInteraction appCompatEditText = onView( withId(R.id.editTextText));
+
+        appCompatEditText.perform(replaceText("PassWithoutSpecial"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+
+        textView.check(matches(withText("You shall not pass!")));
+    }
+
+    @Test
+    public void testCorrectTest(){
+
+        ViewInteraction appCompatEditText = onView( withId(R.id.editTextText));
+
+        appCompatEditText.perform(replaceText("Password123123#$*"));
+
+        ViewInteraction materialButton = onView(withId(R.id.button));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(withId(R.id.textView));
+
+        textView.check(matches(withText("Your password meets the requirements")));
+    }
+
+
+
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/main/java/algonquin/cst2335/evan0285/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/evan0285/MainActivity.java
@@ -2,14 +2,130 @@ package algonquin.cst2335.evan0285;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+/**
+ * @author Chris Evans
+ * @version 1.0
+ * Class that checks password validity
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * this holds the text at the centre of the screen
+     */
+    private TextView tv = null;
+    /**
+     * this holds the edit text
+     */
+    private EditText et = null;
+    /**
+     * this holds the button
+     */
+    private Button btn = null;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView tv = findViewById(R.id.textView);
+        EditText et = findViewById(R.id.editTextText);
+        Button btn = findViewById(R.id.button);
+
+
+        btn.setOnClickListener( clk ->{
+            String password = et.getText().toString();
+            if(checkPasswordComplexity( password )){
+                tv.setText("Your password meets the requirements");
+            }
+            else{
+                tv.setText("You shall not pass!");
+            }
+
+
+        });
+
+    }
+    /**
+     *  This function checks password validity
+     * @param pw the string object we are checking
+     * @return Returns true if password matches validity requirements
+     */
+    boolean checkPasswordComplexity(String pw){
+        boolean foundUpperCase = false;
+        boolean foundLowerCase = false;
+        boolean foundNumber = false;
+        boolean foundSpecial = false;
+
+        for(int i = 0; i < pw.length(); i++){
+            char c = pw.charAt(i);
+
+            if(Character.isUpperCase(c)){
+                foundUpperCase = true;
+            }
+            else if(Character.isLowerCase(c)){
+                foundLowerCase = true;
+            }
+            else if(Character.isDigit(c)){
+                foundNumber = true;
+            }
+            else if(isSpecialCharacter(c)){
+                foundSpecial = true;
+            }
+        }
+
+        if(!foundUpperCase){
+            Toast.makeText(this, "Password is missing an upper case letter", Toast.LENGTH_SHORT);
+
+            return false;
+        }
+        else if(!foundLowerCase){
+            Toast.makeText(this, "Password is missing a lower case letter", Toast.LENGTH_SHORT);
+
+            return false;
+        }
+        else if (!foundNumber){
+            Toast.makeText(this, "Password is missing a number", Toast.LENGTH_SHORT);
+
+            return false;
+        }
+        else if (!foundSpecial){
+            Toast.makeText(this, "Password is missing a special character", Toast.LENGTH_SHORT);
+
+            return false;
+        }
+        else{
+            return true;
+        }
+
+
+
+    }
+    /**
+     * method to check if char is special char
+     * @param c
+     * @return boolean value
+     */
+    boolean isSpecialCharacter(char c){
+        switch (c){
+            case '#':
+            case '?':
+            case '*':
+            case '$':
+            case '%':
+            case '^':
+            case '!':
+            case '@':
+                return true;
+            default:
+                return false;
+        }
     }
 }
